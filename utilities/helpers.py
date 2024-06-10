cf01bd14b6fc318033c1dd6adf39c2b332e6a47b
Karol <weber.karol.86@gmail.com>
2024-06-10 14:56:39 +0200

    refactor() api_call()

--- utilities/helpers.py
+++ utilities/helpers.py
@@ -1,3 +1,4 @@
+import pdb
 from enum import Enum
 import requests
 
@@ -11,22 +12,21 @@ class HttpMethod(Enum):
 
 
 def api_call(method: HttpMethod, url, user=None, data=None):
+    """
+    Make an HTTP request to the specified URL.
+
+    :param method: The HTTP method to use (GET, POST, PUT, PATCH, DELETE).
+    :param url: The URL to make the request to.
+    :param user: (Optional) The user token for authentication (default is None).
+    :param data: (Optional) The JSON data to send in the request body (default is None).
+    :return: The response object.
+    :raises ValueError: If an invalid HTTP method is provided.
+    """
     headers = {"Content-Type": "application/json"}
     if user:
         headers["Cookie"] = f'token={user}'
     if method not in HttpMethod:
         raise ValueError("Invalid HTTP method")
-    if method == HttpMethod.GET:
-        response = requests.get(url=url, headers=headers)
-    elif method == HttpMethod.POST:
-        response = requests.post(url=url, headers=headers, json=data)
-    elif method == HttpMethod.PUT:
-        response = requests.put(url=url, headers=headers, json=data)
-    elif method == HttpMethod.PATCH:
-        response = requests.patch(url=url, headers=headers, json=data)
-    elif method == HttpMethod.DELETE:
-        response = requests.delete(url=url, headers=headers, json=data)
-    else:
-        response = "Invalid HTTP method"
+    response = requests.request(method=method.value, url=url, headers=headers, json=data)
     return response
 
